6a4e328f097e4c15d11b7fa1fd66a4c73e5c0e05
Ami Ganguli <ami.ganguli@gmail.com>
2025-11-14 15:51:22 -0500

    Description shouldn't exist.

--- amplify/functions/hello/resource.ts
+++ amplify/functions/hello/resource.ts
@@ -3,7 +3,7 @@ import { defineFunction } from "@aws-amplify/backend";
 export const hello = defineFunction({
   entry: "./handler.ts",
   name: "hello",
-  description: "A simple hello world function",
+//  description: "A simple hello world function",
 //  runtime: NodeVersion.NODEJS_20,
 //   memory: 1024,
 //   timeout: 30,
